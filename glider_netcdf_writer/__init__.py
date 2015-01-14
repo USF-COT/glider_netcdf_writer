1af31425cfa908e0bfd8375fc7279ef19a0132dc
Michael Lindemuth <lindemuth@gmail.com>
2015-01-14 12:52:24 -0500

    Added documentation.  Need to update tests.

--- glider_netcdf_writer/__init__.py
+++ glider_netcdf_writer/__init__.py
@@ -15,6 +15,8 @@
 import numpy as np
 from netCDF4 import default_fillvals as NC_FILL_VALUES
 from netCDF4 import Dataset
+import sys
+from datetime import datetime
 
 
 def open_glider_netcdf(output_path, mode='w', COMP_LEVEL=1):
@@ -27,6 +29,17 @@ class GliderNetCDFWriter(object):
     """
 
     def __init__(self, output_path, mode='w', COMP_LEVEL=1):
+        """Initializes a Glider NetCDF Writer
+        NOTE: Does not open the file.
+
+        Input:
+        - output_path: Path to new or existing NetCDF file.
+        - mode: 'w' to create or overwrite a NetCDF file.
+                'a' to append to an existing NetCDF file.
+                Default: 'w'
+        - COMP_LEVEL: NetCDF compression level.
+        """
+
         self.nc = None
         self.output_path = output_path
         self.mode = mode
@@ -34,12 +47,22 @@ class GliderNetCDFWriter(object):
         self.datatypes = None
 
     def __setup_qaqc(self):
+        """ Internal function for qaqc variable setup
+        """
+
         # Create array of unsigned 8-bit integers to use for _qc flag values
         self.QC_FLAGS = np.array(range(0, 10), 'int8')
         # Meanings of QC_FLAGS
         self.QC_FLAG_MEANINGS = "no_qc_performed good_data probably_good_data bad_data_that_are_potentially_correc  table bad_data value_changed not_used not_used interpolated_value missing_value"  # NOQA
 
     def __setup_time(self):
+        """ Internal function for time variable setup
+        """
+
+        # Skip creation if this variable already exists
+        if 'time' in self.nc.variables:
+            return
+
         # Setup time dimension
         self.time_dimension = self.nc.createDimension('time', None)
         self.time = self.nc.createVariable(
@@ -88,70 +111,87 @@ class GliderNetCDFWriter(object):
         for key, value in sorted(attrs.items()):
             self.time_qc.setncattr(key, value)
 
-        # Setup time_uv dimension
-        # time_uv: 64 bit float - no _Fill_Value since dimension
-        self.time_uv_dimension = self.nc.createDimension('time_uv', 1)
-        self.time_uv = self.nc.createVariable(
-            'time_uv',
-            'f8',
-            ('time_uv',),
-            zlib=True,
-            complevel=self.COMP_LEVEL
-        )
-        attrs = {
-            'axis': "T",
-            'calendar': 'gregorian',
-            'units': 'seconds since 1970-01-01 00:00:00 UTC',
-            'standard_name': 'time',
-            'long_name': 'Approximate time midpoint of each segment',
-            'observation_type': 'estimated'
-        }
-        for key, value in sorted(attrs.items()):
-            self.time_uv.setncattr(key, value)
+    def __update_history(self):
+        """ Updates the history, date_created, date_modified
+        and date_issued variables
+        """
+
+        # Get timestamp for this access
+        # Cannot use datetime.isoformat()
+        # does not append Z at end of string
+        now_time = datetime.utcnow()
+        time_string = now_time.strftime("%Y-%M-%DT%h:%m:%sZ")
+
+        if 'history' not in self.nc.attrs:
+            history = self.nc.setncattr("history", "")
+            self.nc.setncattr("date_created", time_string)
+        else:
+            history = self.nc.attrs['history']
+
+        self.nc.setncattr("date_modified", time_string)
+        self.nc.setncattr("date_issued", time_string)
+        history += "%s: %s" % (time_string, __file__)
 
     def __enter__(self):
+        """ Opens the NetCDF file. Sets up QAQC and time variables.
+        Updates global history variables.
+        Called at beginning of Python with block.
+        """
+
         self.nc = Dataset(
             self.output_path, self.mode,
             format='NETCDF4_CLASSIC'
         )
 
         self.__setup_qaqc()
-        if 'time' not in self.nc.variables:
-            self.__setup_time()
+        self.__setup_time()
+
+        self.__update_history()
 
         return self
 
     def __exit__(self, type, value, tb):
+        """ Updates bounds and closes file.  Called at end of "with" block
+        """
+
+        self.__update_bounds()
         self.nc.close()
         self.nc = None
 
-    # Warning!
-    # Each file must have different values for the following parameters:
-    # date_created, date_issued, date_modified
-    # geospatial_
-    #     lat_max
-    #     lat_min
-    #     lat_resolution
-    #     lat_units
-    #     lon_max
-    #     lon_min
-    #     lon_resolution
-    #     lon_units
-    #     vertical_max
-    #     vertical_min
-    #     vertical_positive
-    #     vertical_resolution
-    #     vertical_units
-    # history
-    # id
-    # time_coverage_end
-    # time_coverage_resolution
-    # time_coverage_start
     def set_global_attributes(self, global_attributes):
+        """ Sets a dictionary of values as global attributes
+
+        Warning!
+        Each file must have different values for the following parameters:
+        date_created, date_issued, date_modified
+        geospatial_
+            lat_max
+            lat_min
+            lat_resolution
+            lat_units
+            lon_max
+            lon_min
+            lon_resolution
+            lon_units
+            vertical_max
+            vertical_min
+            vertical_positive
+            vertical_resolution
+            vertical_units
+        history
+        id
+        time_coverage_end
+        time_coverage_resolution
+        time_coverage_start
+        """
+
         for key, value in sorted(global_attributes.items()):
             self.nc.setncattr(key, value)
 
     def set_trajectory_id(self, trajectory_id):
+        """ Sets the trajectory ID for this dataset as a variable
+        """
+
         if 'trajectory' not in self.nc.variables:
             self.trajectory_dimension = (
                 self.nc.createDimension('trajectory', 1)
@@ -176,12 +216,16 @@ class GliderNetCDFWriter(object):
 
         trajectory_var[0] = trajectory_id
 
-    # SEGMENT_ID
-    # segment_id: 2 byte integer
-    # kerfoot@marine.rutgers.edu: explicitly specify fill_value when creating
-    # variable so that it shows up as a variable attribute.  Use the default
-    # fill_value based on the data type
     def set_segment_id(self, segment_id):
+        """ Sets the segment ID as a variable
+
+        SEGMENT_ID
+        segment_id: 2 byte integer
+        kerfoot@marine.rutgers.edu: explicitly specify fill_value when creating
+        variable so that it shows up as a variable attribute.  Use the default
+        fill_value based on the data type
+        """
+
         segment_var = self.nc.createVariable(
             'segment_id',
             'i2',
@@ -203,12 +247,16 @@ class GliderNetCDFWriter(object):
 
         segment_var[0] = segment_id
 
-    # PROFILE_ID
-    # profile_id: 2 byte integer
-    # kerfoot@marine.rutgers.edu: explicitly specify fill_value when creating
-    # variable so that it shows up as a variable attribute.  Use the default
-    # fill_value based on the data type.
     def set_profile_ids(self, profile_ids):
+        """ Sets Profile ID as a variable
+
+        PROFILE_ID
+        profile_id: 2 byte integer
+        kerfoot@marine.rutgers.edu: explicitly specify fill_value when creating
+        variable so that it shows up as a variable attribute.  Use the default
+        fill_value based on the data type.
+        """
+
         profile_var = self.nc.createVariable(
             'profile_id',
             'i2',
@@ -231,28 +279,40 @@ class GliderNetCDFWriter(object):
         profile_var[:] = profile_ids
 
     def set_platform(self, platform_attrs):
+        """ Creates a variable that describes the glider
+        """
+
         platform = self.nc.createVariable('platform', 'i1')
 
         for key, value in sorted(platform_attrs.items()):
             platform.setncattr(key, value)
 
     def set_instrument(self, name, attrs):
+        """ Adds a description for a single instrument
+        """
+
         instrument = self.nc.createVariable(name, 'i1')
 
         for key, value in sorted(attrs.items()):
             instrument.setncattr(key, value)
 
     def set_instruments(self, instruments_array):
+        """ Adds a list of instrument descriptions to the dataset
+        """
+
         for description in instruments_array:
             self.set_instrument(description['name'], description['attrs'])
 
     def set_times(self, times):
+        """ Sets all values in the time dimension of the dataset
+        """
+
         self.nc.variables['time'][:] = times
 
-    def set_time_uv(self, time_uv):
-        self.nc.variables['time_uv'][0] = time_uv
-
     def set_datatype(self, key, desc):
+        """ Sets up a datatype description for the dataset
+        """
+
         if 'is_dimension' in desc and desc['is_dimension']:
             return  # Skip independent fields
 
@@ -301,25 +361,76 @@ class GliderNetCDFWriter(object):
                 status_flag_var.setncattr(key, value)
 
     def set_datatypes(self, datatypes):
+        """ Sets an array of datatype descriptions in the dataset
+        """
+
         self.datatypes = {}
         for key, desc in sorted(datatypes.items()):
             self.set_datatype(key, desc)
 
-    def insert_data(self, datatype, data):
-        if datatype in self.datatypes:
-            desc = self.datatypes[datatype]
-            try:
-                if desc['dimension'] == 'time':
-                    self.nc.variables[desc['name']][:] = data
+    def insert_dict(self, line):
+        """ Adds a data point glider_binary_data_reader library to NetCDF
+
+        Input:
+        - line: A dictionary of values where the key is a given
+                <value name>-<units> pair that matches a description
+                in the datatypes.json file.
+        """
+
+        for name, desc in self.datatypes.items():
+            if desc['dimension'] == 'time':
+                if desc['name'] in line:
+                    value = line[desc['name']]
                 else:
-                    # TODO: Fix this hack for values in the time_uv
-                    # dimension
                     value = NC_FILL_VALUES['f8']
-                    for datum in data:
-                        if datum != NC_FILL_VALUES['f8']:
-                            value = datum
-                    self.nc.variables[desc['name']][0] = value
-            except Exception, e:
-                print '%s: %s - %s' % (datatype, data, e)
-        else:
-            print "%s not in datatype" % (datatype)
+                self.nc.variables[desc['name']].append(value)
+            else:
+                if desc['name'] in line:
+                    self.nc.variables[desc['name']] = value
+
+    def __find_min(self, dataset):
+        min_val = -sys.maxint - 1
+        for value in dataset:
+            if value == NC_FILL_VALUES['f8']:
+                continue
+            else:
+                if value < min_val:
+                    min_val = value
+
+        return min_val
+
+    def __find_max(self, dataset):
+        max_val = sys.minint
+        for value in dataset:
+            if value == NC_FILL_VALUES['f8']:
+                continue
+            else:
+                if value > max_val:
+                    max_val = value
+
+        return max_val
+
+    def __update_bounds(self):
+        """ Internal description that updates all global attribute bounds
+        before closing a file.
+        """
+
+        for key, desc in self.datatypes.items():
+            if 'global_bound' in desc:
+                prefix = desc['global_bound']
+                self.nc.ncattrs[prefix + '_min'] = (
+                    self.__find_min(self.nc.variables[desc['name']])
+                )
+                self.nc.ncattrs[prefix + '_max'] = (
+                    self.__find_max(self.nc.variables[desc['name']])
+                )
+                self.nc.ncattrs[prefix + '_units'] = desc['attrs']['units']
+                self.nc.ncattrs[prefix + '_resolution'] = (
+                    desc['attrs']['resolution']
+                )
+                self.nc.ncattrs[prefix + '_accuracy'] = (
+                    desc['attrs']['accuracy']
+                )
+                self.nc.ncattrs[prefix + '_precision'] = (
+                    desc['attrs']['precision']
+                )
