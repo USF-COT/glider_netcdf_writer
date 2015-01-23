87ca9a725ad73bef5f292c6704a6e8d80a8b73a6
Michael Lindemuth <lindemuth@gmail.com>
2015-01-23 13:37:04 -0500

    Updated format of the check script
    Easier to add and remove tests from the pool of test functions now

--- scripts/check_glider_netcdf.py
+++ scripts/check_glider_netcdf.py
@@ -20,6 +20,140 @@ import json
 from netCDF4 import Dataset
 
 
+def test_global_attributes(nc, requirements):
+    """ Tests for required global attributes
+    """
+    retVal = 0
+
+    global_attributes = nc.ncattrs()
+    for req_attribute in requirements['global_attributes']:
+        if req_attribute not in global_attributes:
+            print "Global Attribute Missing: %s" % (req_attribute)
+            retVal += 1
+    return retVal
+
+
+def test_dimensions(nc, requirements):
+    """ Tests for required dimensions
+    """
+    retVal = 0
+
+    for req_dimension in requirements['dimensions']:
+        if req_dimension not in nc.dimensions:
+            print "Dimension Missing: %s" % (req_dimension)
+            retVal += 1
+
+    return retVal
+
+
+def test_required_variables(nc, requirements):
+    """ Tests for required variables
+    """
+    retVal = 0
+
+    for req_variable in requirements['required_variables']:
+        variables = nc.variables
+        if req_variable not in variables:
+            print "Missing required variable %s" % req_variable
+            retVal += 1
+
+    return retVal
+
+
+def test_variable_attributes(nc, requirements):
+    """ Tests for required variable attributes
+    """
+    retVal = 0
+
+    for variable_name in nc.variables:
+        # Skip QC variables
+        if variable_name[-2:] == "qc":
+            continue
+
+        # Ignore configured variables
+        if variable_name in requirements['ignore_variable_check']:
+            continue
+
+        variable = nc.variables[variable_name]
+        # Skip scalar and descriptive variables
+        if variable.size < 2:
+            continue
+
+        var_attrs = nc.variables[variable_name].ncattrs()
+        for req_var_attr in requirements['variable_attributes']:
+            if req_var_attr not in var_attrs:
+                print("Variable attribute %s "
+                      "missing in %s variable" % (req_var_attr, variable_name))
+                retVal += 1
+
+    return retVal
+
+
+def test_qc_variables(nc, requirements):
+    """ Tests that all variables have a corresponding qc variable
+    """
+    retVal = 0
+
+    for variable_name in nc.variables:
+        # Skip QC variables
+        if variable_name[-2:] == "qc":
+            continue
+
+        # Ignore configured variables
+        if variable_name in requirements['ignore_variable_check']:
+            continue
+
+        variable = nc.variables[variable_name]
+        if variable.size < 2:
+            continue
+
+        qc_name = "%s_qc" % variable_name
+        if qc_name not in nc.variables:
+            print("QC variable missing for %s" % variable_name)
+            retVal += 1
+
+    return retVal
+
+
+def test_platform_attributes(nc, requirements):
+    """ Tests for required platform attributes
+    """
+    retVal = 0
+
+    platform_attrs = nc.variables['platform'].ncattrs()
+    for req_platform_attr in requirements['platform_attributes']:
+        if req_platform_attr not in platform_attrs:
+            print "Platform attribute %s missing" % req_platform_attr
+            retVal += 1
+
+    return retVal
+
+
+def test_ctd_attributes(nc, requirements):
+    """ Tests for required ctd attributes
+    """
+    retVal = 0
+
+    ctd_attrs = nc.variables['instrument_ctd'].ncattrs()
+    for req_ctd_attr in requirements['ctd_attributes']:
+        if req_ctd_attr not in ctd_attrs:
+            print "CTD attribute %s missing" % req_ctd_attr
+            retVal += 1
+
+    return retVal
+
+
+test_functions = [
+    test_global_attributes,
+    test_dimensions,
+    test_required_variables,
+    test_variable_attributes,
+    test_qc_variables,
+    test_platform_attributes,
+    test_ctd_attributes
+]
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Verifies that a glider NetCDF file from a provider '
@@ -59,80 +193,8 @@ def main():
     # Initialize return value
     retVal = 0
 
-    # Check Global Attributes
-    global_attributes = nc.ncattrs()
-    for req_attribute in requirements['global_attributes']:
-        if req_attribute not in global_attributes:
-            print "Global Attribute Missing: %s" % (req_attribute)
-            retVal += 1
-
-    # Check dimensions
-    for req_dimension in requirements['dimensions']:
-        if req_dimension not in nc.dimensions:
-            print "Dimension Missing: %s" % (req_dimension)
-            retVal += 1
-
-    # Check for required variables
-    for req_variable in requirements['required_variables']:
-        variables = nc.variables
-        if req_variable not in variables:
-            print "Missing required variable %s" % req_variable
-            retVal += 1
-
-    # Check variable attributes
-    for variable_name in nc.variables:
-        # Skip QC variables
-        if variable_name[-2:] == "qc":
-            continue
-
-        # Ignore configured variables
-        if variable_name in requirements['ignore_variable_check']:
-            continue
-
-        variable = nc.variables[variable_name]
-        # Skip scalar and descriptive variables
-        if variable.size < 2:
-            continue
-
-        var_attrs = nc.variables[variable_name].ncattrs()
-        for req_var_attr in requirements['variable_attributes']:
-            if req_var_attr not in var_attrs:
-                print("Variable attribute %s "
-                      "missing in %s variable" % (req_var_attr, variable_name))
-                retVal += 1
-
-    # Check QC
-    for variable_name in nc.variables:
-        # Skip QC variables
-        if variable_name[-2:] == "qc":
-            continue
-
-        # Ignore configured variables
-        if variable_name in requirements['ignore_variable_check']:
-            continue
-
-        variable = nc.variables[variable_name]
-        if variable.size < 2:
-            continue
-
-        qc_name = "%s_qc" % variable_name
-        if qc_name not in nc.variables:
-            print("QC variable missing for %s" % variable_name)
-            retVal += 1
-
-    # Check Platform Attributes
-    platform_attrs = nc.variables['platform'].ncattrs()
-    for req_platform_attr in requirements['platform_attributes']:
-        if req_platform_attr not in platform_attrs:
-            print "Platform attribute %s missing" % req_platform_attr
-            retVal += 1
-
-    # Check CTD Attributes
-    ctd_attrs = nc.variables['instrument_ctd'].ncattrs()
-    for req_ctd_attr in requirements['ctd_attributes']:
-        if req_ctd_attr not in ctd_attrs:
-            print "CTD attribute %s missing" % req_ctd_attr
-            retVal += 1
+    for test_fun in test_functions:
+        retVal += test_fun(nc, requirements)
 
     if retVal == 0:
         print "PASS"
